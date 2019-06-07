452587131f5bf00e5759b7645f4368624d66794c
Saurabh <saurabh.v.kumar@nl.abnamro.com>
2019-06-07 18:05:04 +0200

    Added travis yml

--- server/keys.js
+++ server/keys.js
@@ -1,10 +1,10 @@
-module.exports = { 
+module.exports = {
     redisHost: process.env.REDIS_HOST,
     redisPort: process.env.REDIS_PORT,
-    pgUser: 'postgres',
-    pgHost: 'redis',
-    pgDatabase: 'postgres',
-    pgPassword: '',
-    pgPort: 5432
-};
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+  };
   
